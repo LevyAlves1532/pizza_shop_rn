b3b47dbab1337b899d7539555a5f286b5225a7fa
Lêvy <levy.pereiraA1532@gmail.com>
2023-11-04 11:26:50 -0300

    Resolvendo bug da tela de carrinho

--- src/screens/Cart.tsx
+++ src/screens/Cart.tsx
@@ -1,5 +1,5 @@
 // LIBs
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { StyleSheet, View, Text, ScrollView } from "react-native";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 
@@ -19,17 +19,29 @@ import { BORDERRADIUS, COLORS, SPACINGS } from "../themes/theme";
 export function CartScreen({ navigation }: any) {
   const tabBarHeight = useBottomTabBarHeight();
 
+  const scrollView = useRef<any>(null);
+
   const CartList = useStore((state: any) => state.CartList);
   const CartPrice = useStore((state: any) => state.CartPrice);
   const decrementSizeQuantity = useStore((state: any) => state.decrementSizeQuantity);
   const incrementSizeQuantity = useStore((state: any) => state.incrementSizeQuantity);
   const calculateTotalPrice = useStore((state: any) => state.calculateTotalPrice);
 
+  useEffect(() => {
+    if (CartList.length === 0) {
+      scrollView.current?.scrollTo({
+        y: 0,
+        animated: true,
+      });
+    }
+  }, [CartList])
+
   return (
     <View style={styles.ScreenContainer}>
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.ScrollViewContainer}
+        ref={scrollView}
       >
         <Header 
           iconName="shopping-cart"
